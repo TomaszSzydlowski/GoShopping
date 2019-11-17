deb26e85143b777936dbab8885af624cad6468c4
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-11-17 21:43:48 +0100

    added PreparationTime column to dish

--- GoShopping/Db/Dish.cs
+++ GoShopping/Db/Dish.cs
@@ -8,5 +8,6 @@ namespace GoShopping
         public int DishId { get; set; }
         public String Name { get; set; }
         public virtual IList<Ingredient> Ingredients { get; set; }
+        public int PreparationTime  { get; set; }
     }
 }
