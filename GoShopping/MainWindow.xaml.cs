e1820bcb2c7b3f58b633afea16f283721a299a06
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-11-19 19:23:10 +0100

    Dynamic solution

--- GoShopping/MainWindow.xaml.cs
+++ GoShopping/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GoShopping
 {
@@ -13,44 +14,58 @@ namespace GoShopping
         public MainWindow()
         {
             InitializeComponent();
+            dynamicCheckBox();
+        }
+
+        private void dynamicCheckBox()
+        {
+            GoShoppingDbContext context = new GoShoppingDbContext();
+            var itemList = context.Dishes.ToList();
+
+            foreach (var item in itemList)
+            {
+                CheckBox chk=new CheckBox();
+                chk.Content = item.Name.ToString();
+                st1.Children.Add(chk);
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // Create demo: Create a User instance and save it to the database
-            Dish newDish = new Dish { Name = "Spagetti" };
-            context.Dishes.Add(newDish);
-            context.SaveChanges();
+            //// Create demo: Create a User instance and save it to the database
+            //Dish newDish = new Dish { Name = "Spagetti" };
+            //context.Dishes.Add(newDish);
+            //context.SaveChanges();
 
-            // Create demo: Create a Task instance and save it to the database
-            Ingredient newIngredient = new Ingredient()
-            {
-                Name = "Makaron",
-                Quantity = 200,
-            };
-            context.Ingredients.Add(newIngredient);
-            context.SaveChanges();
+            //// Create demo: Create a Task instance and save it to the database
+            //Ingredient newIngredient = new Ingredient()
+            //{
+            //    Name = "Makaron",
+            //    Quantity = 200,
+            //};
+            //context.Ingredients.Add(newIngredient);
+            //context.SaveChanges();
 
-            Unit newUnit = new Unit
-            {
-                Name = "g"
-            };
+            //Unit newUnit = new Unit
+            //{
+            //    Name = "g"
+            //};
 
-            context.Units.Add(newUnit);
-            context.SaveChanges();
+            //context.Units.Add(newUnit);
+            //context.SaveChanges();
 
-            // Association demo: Assign task to user
-            newIngredient.Dish = newDish;
-            newIngredient.Unit = newUnit;
-            context.SaveChanges();
+            //// Association demo: Assign task to user
+            //newIngredient.Dish = newDish;
+            //newIngredient.Unit = newUnit;
+            //context.SaveChanges();
 
-            // Read demo: find incomplete tasks assigned to user 'Anna'
-            var query = from d in context.Dishes
-                        where d.Name == "Spagetti"
-                        select d;
+            //// Read demo: find incomplete tasks assigned to user 'Anna'
+            //var query = from d in context.Dishes
+            //            where d.Name == "Spagetti"
+            //            select d;
 
 
-            dataGrid1.ItemsSource = query.ToList();
+            //dataGrid1.ItemsSource = query.ToList();
         }
     }
 }
