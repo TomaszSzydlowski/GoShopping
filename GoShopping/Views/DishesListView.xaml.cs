c54c3e610c6d2951f0c4de5835caa57e73dc6e75
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-12-18 22:18:36 +0100

    Added grid in code and NumericBoxes

--- GoShopping/Views/DishesListView.xaml.cs
+++ GoShopping/Views/DishesListView.xaml.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections.Generic;
-using GoShopping.Models;
+﻿using GoShopping.Models;
 using GoShopping.ViewModels;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace GoShopping.Views
 {
@@ -15,12 +14,29 @@ namespace GoShopping.Views
     public partial class DishesListView : UserControl
     {
         GoShoppingDbContext _dbContext = new GoShoppingDbContext();
-        List<string> _temporaryListOfSelectedDishes=new List<string>();
+        List<string> dishes = new List<string>();
+        List<string> _temporaryListOfSelectedDishes = new List<string>();
 
         public DishesListView()
         {
             InitializeComponent();
-            DataContext = _dbContext.Dishes.Select(x => x.Name).ToList();
+            dishes = _dbContext.Dishes.Select(x => x.Name).ToList();
+            DataContext = dishes;
+            SetGrid();
+        }
+
+        private void SetGrid()
+        {
+            for (int i = 0; i < dishes.Count; i++)
+            {
+                NumericBoxGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(22.2) });
+            }
+            NumericBoxGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            NumericBoxGrid.ColumnDefinitions.Add(new ColumnDefinition {  Width = new GridLength(25)});
+            NumericBoxGrid.ColumnDefinitions.Add(new ColumnDefinition {  Width = GridLength.Auto });
+            Grid.SetColumn(StackPanelListView, 1);
+            Grid.SetRow(StackPanelListView, 0);
+            Grid.SetRowSpan(StackPanelListView, dishes.Count+1);
         }
 
         private void OnUncheckItem(object sender, RoutedEventArgs e)
@@ -31,16 +47,36 @@ namespace GoShopping.Views
             DishesListViewModel.SelectedDishes = listView.SelectedItems;
 
             _temporaryListOfSelectedDishes.Remove(DishesListViewModel.SelectedItem);
+
+            var numberOfRow = dishes.IndexOf(DishesListViewModel.SelectedItem);
+            if (NumericBoxGrid.Children.OfType<TextBox>().Any(x => x.Name.Equals($"NumericBox{numberOfRow + 1}"))) { NumericBoxGrid.Children.Remove(NumericBoxGrid.Children.OfType<TextBox>().FirstOrDefault(x => x.Name.Equals($"NumericBox{numberOfRow + 1}"))); }
         }
 
         private void checkedListView_Checked(object sender, RoutedEventArgs e)
         {
-            var listView = (ListView) sender;
+            var listView = (ListView)sender;
 
             DishesListViewModel.SelectedItem = listView.SelectedItems.Cast<string>().ToList().LastOrDefault();
             DishesListViewModel.SelectedDishes = listView.SelectedItems;
 
             _temporaryListOfSelectedDishes.Add(DishesListViewModel.SelectedItem);
+
+            var numberOfRow = dishes.IndexOf(DishesListViewModel.SelectedItem);
+            if (!NumericBoxGrid.Children.OfType<TextBox>().Any(x => x.Name.Equals($"NumericBox{numberOfRow + 1}"))) { CreateNumericBox(numberOfRow); }
+        }
+
+        private void CreateNumericBox(int numberOfRow)
+        {
+            TextBox txt = new TextBox();
+            txt.Name = $"NumericBox{numberOfRow + 1}";
+            txt.Text = "1";
+            txt.FontFamily = new FontFamily("Segoe UI Light");
+            txt.FontSize = 16;
+            txt.TextAlignment = TextAlignment.Center;
+            txt.Margin = new Thickness(0, 1, 0, 1);
+            Grid.SetColumn(txt, 0);
+            Grid.SetRow(txt, numberOfRow);
+            NumericBoxGrid.Children.Add(txt);
         }
 
         private void EditMenu_Click(object sender, RoutedEventArgs e)
